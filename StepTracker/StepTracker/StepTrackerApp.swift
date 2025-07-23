5835cbef55837c195e3fa2e8aed20b667ffc85c6
Adrian Castañeda Rodriguez <ing.adrian.castaneda@gmail.com>
2025-07-22 22:08:02 -0700

    created xcode project

---
+++ StepTracker/StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Adrian Castañeda on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
