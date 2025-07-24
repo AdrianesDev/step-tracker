be4a527a79109aaeb5195aaaa58e34aab371da8c
Adrian Castañeda Rodriguez <ing.adrian.castaneda@gmail.com>
2025-07-24 04:30:19 -0700

    mock data on simulator

--- StepTracker/StepTracker/DashboardView.swift
+++ StepTracker/StepTracker/DashboardView.swift
@@ -24,6 +24,7 @@ enum HealthMetricContext: CaseIterable, Identifiable {
 }
 
 struct DashboardView: View {
+    @Environment(HealthKitManager.self) private var hkManager
     @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
     @State private var isShowingPermissionPrimingSheet: Bool = false
     @State private var selectedStat: HealthMetricContext = .steps
@@ -102,8 +103,10 @@ struct DashboardView: View {
                 
             }
             .padding()
-            .onAppear {
+            .task {
+//                await hkManager.addSimulatorData()
                 isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
+                
             }
             .navigationTitle("Dashboard")
             .navigationDestination(for: HealthMetricContext.self) { metric in
